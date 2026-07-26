1bb71c4212b0f2165358d10b24d0cdb799c0ad1f
Lucas S. Jesus <lucasnetprog@gmail.com>
2026-07-25 23:32:00 -0300

    feat: Adding the dotenv and jwt package

--- core-api/bootstrap/app.php
+++ core-api/bootstrap/app.php
@@ -5,6 +5,9 @@ use System\Services\Response\ResponseResolver as Response;
 
 try{
     
+    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__,2));
+    $dotenv->load();
+
     $application = new ApplicationHandle();
     
     $application->handle();
